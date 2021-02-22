4b0e94e59fa1b2b14dcd718ece220e88f25327f0
Evgeny <zhenya.leshhinskij@gmail.com>
2021-02-22 21:47:06 +0300

    made models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    img = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+# class Comment(models.Model):
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+#     title = models.CharField(max_length=100)
+#     text = models.TextField()
+#
+#     def __str__(self):
+#         return self.title
+
+
+class Comment1(models.Model):
+    post = models.OneToOneField(Post, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.title
--- blogs/settings.py
+++ blogs/settings.py
@@ -55,9 +55,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'blogs.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -65,9 +62,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -85,9 +79,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -98,8 +89,5 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
